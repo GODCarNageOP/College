4620b4adc91d5ede2daf3784a02103e6e0367197
GODCarNageOP <yashvharale@gmail.com>
2023-07-18 22:14:32 +0530

    admission page added

--- src/components/CollegeDatatable.tsx
+++ src/components/CollegeDatatable.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
-// import { DataGrid } from '@mui/x-data-grid';
-// import { userColumns, userRows } from '../constants/collegedata';
+import { DataGrid } from '@mui/x-data-grid';
+import { userColumns, userRows } from '../constants/collegedata';
 
 const CollegeDataTable: React.FC = () => {
   return (
- <div className="collegedatatable h-[100vh] w-[80%] bg-[#0c0c0c] text-white">
+ <div className="collegedatatable h-[80%] p-2 bg-[#0c0c0c] text-white mx-10">
+  <div className="datatable flex items-center justify-center h-[100vh] mt-52 mb-20">
+  <DataGrid
+      className="datagrid"
+      rows={userRows}
+      columns={userColumns}
+      pageSize={9}
+      rowsPerPageOptions={[9]}
+      checkboxSelection
+      style={{color: "white", backgroundColor: "transparent"}}
+    />
+ </div>
  </div>
   );
 };
